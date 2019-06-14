5338ef25040676738090d5263c81457469fa38c5
gudzy <gustdyn@gmail.com>
2019-06-14 11:41:39 +0100

    LEKSE-40 Add hero section to landingpage

--- src/views/LandingPage/LandingPage.tsx
+++ src/views/LandingPage/LandingPage.tsx
@@ -1,11 +1,13 @@
 // Sections for this page
 import React, { Component } from 'react';
 import SectionQuestions from './Sections/SectionQuestions';
+import SectionHero from './Sections/SectionHero';
 
 class LandingPage extends Component {
   render(){
     return(
       <div >
+        <SectionHero />
         <SectionQuestions />
       </div>
     );
---
+++ src/views/LandingPage/Sections/SectionHero.tsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import '../../../styles/SectionHero.css';
+
+
+const message = {
+    header: "Gratis, trygt og anonymt for deg på ungdomsskolen og videregående.",
+    tips: "Hvis det tar lang tid å få videohjelp anbefaler vi å prøve vanlig chat i stedet. Det går ofte raskere!"
+}
+
+class SectionQuestions extends Component {
+
+    
+  render() {
+    return (
+      <div className="hero">
+        <div className="hero--description">
+          <span style={{color: '#8b51c6'}}>Gratis, trygt </span> og <span style={{color: '#8b51c6'}}>anonymt</span> for deg på ungdomsskolen og videregående.
+        </div>
+        <div className="hero-tips">
+          {message.tips}
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default SectionQuestions;
