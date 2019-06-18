adcbe7b3d563a6af6c3e7c01e11a02ba727ced17
Caroline Sandsbråten <carolinesandsbraten@gmail.com>
2019-06-18 12:48:34 +0200

    Change use from class component to functional component

--- src/views/QA/QA.tsx
+++ src/views/QA/QA.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import { get } from '../../services/api-service';
+import { getQuestionList } from '../../services/api-service';
 import QAList from '../../ui/components/QAList';
 import IQuestion from '../../interfaces/IQuestion';
 
@@ -8,26 +8,14 @@ interface IState {
   questions: IQuestion[];
 }
 
-export default class QA extends React.Component<{}, IState> {
-  public constructor(state: IState) {
-    super(state);
-    this.state = {
-      questions: [] as IQuestion[],
-    };
-  }
+export const QA = () => {
+  const [questions, setQuestions] = React.useState([] as IQuestion[]);
 
-  public componentDidMount(): void {
-    get('questions')
-      .then(res => {
-        console.log(res);
-        this.setState({
-          questions: res.data,
-        });
-      })
-      .then(() => console.log(this.state.questions));
-  }
+  React.useEffect(() => {
+    getQuestionList().then(setQuestions);
+  });
 
-  public render(): React.ReactNode {
-    return <div>{QAList(this.state.questions)}</div>;
-  }
-}
+  return <div>{questions && QAList(questions)}</div>;
+};
+
+export default QA;
