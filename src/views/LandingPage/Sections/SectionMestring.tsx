77e8c00e4e2632152489ee2bda633a69d1ab8953
gudzy <gustdyn@gmail.com>
2019-06-25 14:07:25 +0100

    LEKSE-61 Add svg image to mestring section

--- src/views/LandingPage/Sections/SectionMestring.tsx
+++ src/views/LandingPage/Sections/SectionMestring.tsx
@@ -6,31 +6,40 @@ import '../../../styles/LandingPage.less';
 
 const SectionMestring = () => {
   return (
-    <div>
-      <div className="mestring">
-        <a href="/mestring" style={{ textDecoration: 'none', color: 'black' }}>
-          <div className="mestring--header">Mestring og motivasjon</div>
-        </a>
+    <div className="help">
+      <div>
+        <div className="mestring">
+          <a
+            href="/mestring"
+            style={{ textDecoration: 'none', color: 'black' }}
+          >
+            <div className="mestring--header">Mestring og motivasjon</div>
+          </a>
 
-        <form className="mestring--form">
-          <div className="mestring--form--header">
-            Se hvilke temaer vi kan hjelpe deg med
-          </div>
-          <Dropdown
-            options={[]}
-            placeholder="F.eks motivasjon, læringsmetoder"
-          />
-        </form>
+          <form className="mestring--form">
+            <div className="mestring--form--header">
+              Se hvilke temaer vi kan hjelpe deg med
+            </div>
+            <Dropdown
+              options={[]}
+              placeholder="F.eks motivasjon, læringsmetoder"
+            />
+          </form>
+        </div>
+        <div className="cross-my-heart">
+          Trenger du noen å snakke med?{' '}
+          <a
+            href="https://korspaahalsen.rodekors.no/"
+            className="cross-my-heart--link"
+          >
+            Kors på halsen
+          </a>
+        </div>
       </div>
-      <div className="cross-my-heart">
-        Trenger du noen å snakke med?{' '}
-        <a
-          href="https://korspaahalsen.rodekors.no/"
-          className="cross-my-heart--link"
-        >
-          Kors på halsen
-        </a>
-      </div>
+      <img
+        src={require('../../../assets/images/figure_2.svg')}
+        className="help--image"
+      />
     </div>
   );
 };
