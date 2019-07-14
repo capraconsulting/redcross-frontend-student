54941d9b0ee45a7063f74958b9f99f4e9dde151f
gudzy <gustdyn@gmail.com>
2019-07-14 21:46:31 +0100

    LEKSE-20 Add description

--- src/views/Leksehjelp/LeksehjelpPage.tsx
+++ src/views/Leksehjelp/LeksehjelpPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const LeksehjelpPage = () => {
-  return <div className="content">Leksehjelp</div>;
+  return <div className="content">Leksehjelp page</div>;
 };
 
 export default LeksehjelpPage;
