34ac022b985e65c987cb76e616d975c86c3399c2
gudzy <gustdyn@gmail.com>
2019-08-08 14:38:44 +0100

    Add chat file styling

--- src/views/Chat/Sections/ChatInput.tsx
+++ src/views/Chat/Sections/ChatInput.tsx
@@ -8,6 +8,7 @@ import { uploadFileToAzureBlobStorage } from '../../../services/azure-service';
 
 //Styles
 import '../../../styles/ChatInput.less';
+import Cross from '../../../assets/Cross';
 
 //Components
 import { IconButton } from '../../../ui/components';
@@ -80,32 +81,25 @@ const ChatInput = (props: IProps) => {
 
   const FileList = () => {
     return (
-      <ul className="filelist">
-        {tempFiles.map((file, index) => {
-          const { name } = file;
-          return (
-            <li className="element" key={index}>
-              <Zoom>
-                <span>
-                  <a
-                    className="filelist-ankertag"
-                    href={URL.createObjectURL(file)}
-                    title={name}
-                    download={name}
-                  >
-                    {name}{' '}
-                  </a>
-                  <IconButton
-                    onClick={() => {
-                      setTempFiles(tempFiles.filter((_, i) => i !== index));
-                    }}
-                  />{' '}
-                </span>
-              </Zoom>
-            </li>
-          );
-        })}
-      </ul>
+      <div className="subject--list">
+        {tempFiles &&
+          tempFiles.map((file, index) => {
+            const { name } = file;
+            return (
+              <div key={index} className="subject--list-element">
+                <p>{name}</p>
+                <button
+                  className="leksehjelp--button-close chip-btn-x"
+                  onClick={() => {
+                    setTempFiles(tempFiles.filter((_, i) => i !== index));
+                  }}
+                >
+                  <Cross color="#8b51c6" />
+                </button>
+              </div>
+            );
+          })}
+      </div>
     );
   };
 
@@ -123,62 +117,64 @@ const ChatInput = (props: IProps) => {
   }
 
   return (
-    <div {...getRootProps({ className: 'dropzone' })}>
-      <input {...getInputProps()} />
-      <input
-        onChange={event => {
-          let { files } = event.target;
-          let newFiles = [] as any;
-          let steps = (files && files.length) || 0;
-          for (var i = 0; i < steps; i++) {
-            let item = (files && files.item(i)) || 'null';
-            newFiles.push(item);
-          }
-          files && setTempFiles([...tempFiles, ...newFiles]);
-        }}
-        type="file"
-        name="attachment"
-        id="msg-file-input"
-        accept="image/*|.pdf|.doc|.docx"
-        className="file"
-      />
-      <div className={'message-form-container'}>
-        <form className={'message-form'}>
-          <button
-            type="button"
-            className="upload"
-            onClick={() => openFileDialog()}
-          >
-            <input
-              type="file"
-              id="file-dialog"
-              className="input-file"
-              accept="image/*|.pdf|.doc|.docx|.csv"
-              onChange={event => {
-                let { files } = event.target;
-                let newFiles = [] as any;
-                let steps = (files && files.length) || 0;
-                for (var i = 0; i < steps; i++) {
-                  let item = (files && files.item(i)) || 'null';
-                  newFiles.push(item);
-                }
-                files && setTempFiles([...tempFiles, ...newFiles]);
-              }}
-            />
-            <span className="plus">+</span>
-            <div className="tooltip">
-              Hvis du sender et vedlegg, må du gjerne fjerne navnet ditt eller
-              andre ting fra dokumentet som kan identifisere deg.
-            </div>
-          </button>
-          {renderInput()}
-          <button onClick={handleSubmit} className={'send-message'}>
-            <svg width="30px" height="30px" viewBox="0 0 30 30">
-              <polygon className="arrow" points="30 15 0 30 5.5 15 0 0" />
-            </svg>
-          </button>
-        </form>
-        <FileList />
+    <div>
+      <FileList />
+      <div {...getRootProps({ className: 'dropzone' })}>
+        <input {...getInputProps()} />
+        <input
+          onChange={event => {
+            let { files } = event.target;
+            let newFiles = [] as any;
+            let steps = (files && files.length) || 0;
+            for (var i = 0; i < steps; i++) {
+              let item = (files && files.item(i)) || 'null';
+              newFiles.push(item);
+            }
+            files && setTempFiles([...tempFiles, ...newFiles]);
+          }}
+          type="file"
+          name="attachment"
+          id="msg-file-input"
+          accept="image/*|.pdf|.doc|.docx"
+          className="file"
+        />
+        <div className={'message-form-container'}>
+          <form className={'message-form'}>
+            <button
+              type="button"
+              className="upload"
+              onClick={() => openFileDialog()}
+            >
+              <input
+                type="file"
+                id="file-dialog"
+                className="input-file"
+                accept="image/*|.pdf|.doc|.docx|.csv"
+                onChange={event => {
+                  let { files } = event.target;
+                  let newFiles = [] as any;
+                  let steps = (files && files.length) || 0;
+                  for (var i = 0; i < steps; i++) {
+                    let item = (files && files.item(i)) || 'null';
+                    newFiles.push(item);
+                  }
+                  files && setTempFiles([...tempFiles, ...newFiles]);
+                }}
+              />
+              <span className="plus">+</span>
+              <div className="tooltip">
+                Hvis du sender et vedlegg, må du gjerne fjerne navnet ditt eller
+                andre ting fra dokumentet som kan identifisere deg.
+              </div>
+            </button>
+            {renderInput()}
+            <button onClick={handleSubmit} className={'send-message'}>
+              <svg width="30px" height="30px" viewBox="0 0 30 30">
+                <polygon className="arrow" points="30 15 0 30 5.5 15 0 0" />
+              </svg>
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
