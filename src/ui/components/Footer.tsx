2fbca04d6ee9ec3db548439505a6bd448bf35f05
gudzy <gustdyn@gmail.com>
2019-08-08 11:58:58 +0100

    Adjust footer icons styling

--- src/ui/components/Footer.tsx
+++ src/ui/components/Footer.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
 import Zoom from 'react-reveal/Zoom';
+import Roll from 'react-reveal/Roll';
 import { SocialIcon } from 'react-social-icons';
 
 //Styles
 import '../../styles/Footer.less';
 
 const Footer = () => (
-  <div className="footer">
-    <div className="footer--container">
+  <div>
+    <Zoom>
       <a href="https://www.rodekors.no/" className="footer--container--logo">
         <img
-          className="footer--rk_logo"
+          className="rk-icon"
           src={require('../../assets/images/rk_logo.png')}
         />
       </a>
-      <a className="footer--container--icon" id="footer--content">
+    </Zoom>
+    <Zoom>
+      <a className="facebook-icon" id="footer--content">
         <SocialIcon
           url="https://www.facebook.com/digitalleksehjelp/"
           style={{ height: 40, width: 40 }}
         />
       </a>
-    </div>
+    </Zoom>
   </div>
 );
 
