e256531301cd56fb3bf28c22566fcf4685bcc709
sandraskars <sandraskarshaug@gmail.com>
2020-01-15 13:39:55 +0100

    Upgrade image-webpack-loader version

--- webpack.js
+++ webpack.js
@@ -63,15 +63,11 @@ module.exports = env => {
                   enabled: false,
                 },
                 pngquant: {
-                  quality: '65-90',
+                  quality: [0.65, 0.90],
                   speed: 4,
                 },
                 gifsicle: {
                   interlaced: false,
-                },
-                // the webp option will enable WEBP
-                webp: {
-                  quality: 75,
                 },
               },
             },
