90d160c226cbbf629e024b1233a4fb690e92cb1d
sandraskars <sandraskarshaug@gmail.com>
2020-01-15 14:29:43 +0100

    Add paragraphs to volunteer text and fix ordering of text and images

--- src/views/Frivillige/Sections/SectionInfo.tsx
+++ src/views/Frivillige/Sections/SectionInfo.tsx
@@ -8,15 +8,17 @@ const SectionInfo = () => {
     <div className="volunteersPage--section">
       <div className="volunteersPage--section--div">
         <div className="volunteer--header">Våre frivillige</div>
-        <p className="volunteer--text" id="volunteer--text">
+        <p className="volunteer--text">
           På samme måte som de fleste andre aktiviteter i Røde Kors, er Digital
           Leksehjelp drevet av frivillige. Våre frivillige er trygge
           voksenpersoner som bruker sine fagkunnskaper og sitt engasjement til å
           støtte elever med skolearbeidet, og å legge til rette for mestring,
-          motivasjon og lærelyst. Digital Leksehjelp er en trygg digital
-          plattform som bygger på Røde Kors-prinsippene humanitet, upartiskhet,
-          nøytralitet, uavhengighet, frivillighet, enhet og universalitet. Les
-          mer om prinsippene{' '}
+          motivasjon og lærelyst.
+        </p>
+        <p className="volunteer--text">
+          Digital Leksehjelp er en trygg digital plattform som bygger på Røde
+          Kors-prinsippene humanitet, upartiskhet, nøytralitet, uavhengighet,
+          frivillighet, enhet og universalitet. Les mer om prinsippene{' '}
           <a
             href="https://www.rodekors.no/om/bevegelsen/"
             className="volunteer--text--colored"
@@ -26,6 +28,8 @@ const SectionInfo = () => {
             her
           </a>
           {'. '}
+        </p>
+        <p className="volunteer--text">
           Vi har omlag 60 aktive frivillige, fordelt på kontorer tilknyttet
           Oslo, Bergen og Stavanger Røde Kors. Våre frivillige har gjennomført
           et obligatorisk introduksjonskurs til Røde Kors og et obligatorisk
