5cd2d8de317b03705f29cbf59da535fd2a08b7d1
sandraskars <sandraskarshaug@gmail.com>
2020-01-20 08:30:19 +0100

    WIP display content in two columns instead of one

--- src/views/Landing/Sections/SectionFrivillig.tsx
+++ src/views/Landing/Sections/SectionFrivillig.tsx
@@ -9,20 +9,6 @@ const SectionFrivillig = (props: RouteComponentProps) => {
   const { history } = props;
   return (
     <div className="volunteer">
-      <div className="volunteer--header">Våre frivillige</div>
-      <p className="volunteer--text" id="sectionVolunteer--text">
-        På samme måte som de fleste andre aktiviteter i Røde Kors, er Digital
-        Leksehjelp drevet av frivillige. Våre frivillige er trygge
-        voksenpersoner som bruker sine fagkunnskaper og sitt engasjement til å
-        gjøre elevers skolehverdag bedre og legge til rette for mestring,
-        motivasjon og lærelyst.{' '}
-      </p>
-      <a
-        onClick={() => history.push('frivillige')}
-        className="volunteer--text--colored"
-      >
-        Les mer...
-      </a>
       <div className="volunteer--image">
         <a
           className="volunteer--image--badge_new"
@@ -34,7 +20,23 @@ const SectionFrivillig = (props: RouteComponentProps) => {
           src={require('../../../assets/images/volunteers_1.jpg')}
           className="volunteer--image--content"
           alt="frivillig"
-        ></img>
+        />
+      </div>
+      <div className="volunteer--information">
+        <div className="volunteer--header">Våre frivillige</div>
+        <p className="volunteer--text" id="sectionVolunteer--text">
+          På samme måte som de fleste andre aktiviteter i Røde Kors, er Digital
+          Leksehjelp drevet av frivillige. Våre frivillige er trygge
+          voksenpersoner som bruker sine fagkunnskaper og sitt engasjement til å
+          gjøre elevers skolehverdag bedre og legge til rette for mestring,
+          motivasjon og lærelyst.{' '}
+        </p>
+        <a
+          onClick={() => history.push('frivillige')}
+          className="volunteer--text--colored"
+        >
+          Les mer...
+        </a>
       </div>
     </div>
   );
